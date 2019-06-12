e361b586451b03bde0ff2b06f0d9457a046c1646
generic-tag-3052 <christopherchoi97@gmail.com>
2019-06-12 15:13:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,22 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
-function
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  }
+  else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
